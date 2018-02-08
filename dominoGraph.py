3b1b78a3168fb4539d118ee9b03fae795749e577
Finkoy <catropital@gmail.com>
2018-02-08 12:30:36 -0600

    Has perfect matching finding capabilities

--- dominoGraph.py
+++ dominoGraph.py
@@ -5,7 +5,7 @@ import networkx as nx
 Surface graph for the Aztec diamond
 Use to find maximum weighted perfect matching
 '''''''''''''''''''''''''''''''''''''''
-class SurfaceGraph:
+class DominoGraph:
 	def __init__(self, n):
 		self.n = 1
 		self.G = nx.Graph()
@@ -62,7 +62,8 @@ class SurfaceGraph:
 			self.G.add_edge(botton_left, (botton_left[0], botton_left[1] - 1))
 			self.mapping[(botton_left[0] - 1, botton_left[1])] = (botton_left[0] - 1, botton_left[1])
 			self.mapping[(botton_left[0], botton_left[1] - 1)] = (botton_left[0], botton_left[1] - 1)
-			
+
+
 		self.n += 1
 		#connect middle nodes
 		self.G.add_edge((0.5, self.n - 0.5), (-0.5, self.n - 0.5))
@@ -70,8 +71,33 @@ class SurfaceGraph:
 		self.G.add_edge((-self.n + 0.5, 0.5), ((-self.n + 0.5), -0.5))
 		self.G.add_edge((0.5, -self.n + 0.5), (-0.5, -self.n + 0.5))
 
+	'''
+	Finds a perfect matching. We are assuming the graph is unweighted for now.
+	maxcardinality is set to true for the needs of this project
+	'''
+	def perfect_matching(self):
+		return nx.max_weight_matching(self.G, maxcardinality = True)
 
-
+	'''
+	Gets the edges we must avoid in the surface graph.
+	Returns a hashset for good query time
+	'''
+	def get_avoid_edges(self):
+		hs = set()
+		matching = self.perfect_matching()
+		for match in matching:
+			(x1, y1), (x2, y2) = match
+			if(x1 == x2):
+				#vertical edge
+				bigY = y1 if (y1 >= y2) else y2
+				smallY = y1 if (y1 < y2) else y2
+				hs.add(((x1 - 0.5, smallY + 0.5), (x2 + 0.5, bigY - 0.5)))
+			else:
+				#horiztonal edge 
+				bigX = x1 if (x1 >= x2) else x2
+				smallX = x1 if (x1 < x2) else x2
+				hs.add(((smallX + 0.5, y1 - 0.5), (bigX - 0.5, y1 + 0.5)))
+		return hs
 			
 	'''
 	Draws the graph
@@ -83,7 +109,5 @@ class SurfaceGraph:
 		nx.draw_networkx(self.G, pos= self.mapping, ax = ax, with_labels = False, node_size = 20)
 		plt.show()
 
-G = SurfaceGraph(5)
-G.draw()
-
-
+#G = DominoGraph(5)
+#G.draw()
