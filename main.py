1421aae74eef61460e630f36a9c9c226d3481b4b
Finkoy <catropital@gmail.com>
2018-02-12 20:50:46 -0600

    working on height function, still buggy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import dominoGraph
 import vertexGraph
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 '''
 Function to visualize the domino tiling on our Aztec diamond
@@ -13,4 +16,21 @@ def visualize(n):
 		vg.remove_edge(edge)
 	vg.draw()
 
-visualize(5)
+
+def height(n):
+	dg = dominoGraph.DominoGraph(n)
+	vg = vertexGraph.VertexGraph(n)
+	avoid_edges = dg.get_avoid_edges()
+	X, Y, Z = vg.height_map(avoid_edges)
+	
+	for i in range(len(X)):
+		print(X[i], Y[i], Z[i])
+
+	fig = plt.figure()
+	ax = fig.gca(projection='3d')
+	ax.plot_trisurf(X, Y, Z, linewidth=0.2, antialiased=True)
+	plt.show()
+
+
+#visualize(2)
+height(153)
